a482594e3479fb3360f3e02d53f5ec9fb1554164
ksingh <ksinghofficial@proton.me>
2023-09-10 12:11:22 +0530

    fixed deployment issues

--- next.config.js
+++ next.config.js
@@ -1,10 +1,49 @@
-/** @type {import('next').NextConfig} */
-// const nextConfig = {}
+// module.exports = {
+//   eslint: {
+//     // Warning: This allows production builds to successfully complete even if
+//     // your project has ESLint errors.
+//     ignoreDuringBuilds: true,
+//   },
+// }
 
-// module.exports = nextConfig
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-    enabled: process.env.ANALYZE === 'true',
-  })
-  module.exports = withBundleAnalyzer({
-    // your Next.js configuration
-  })
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  output: 'export',
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  // trailingSlash: true,
+ 
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
+ 
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
+}
+ 
+module.exports = nextConfig
+
+// module.exports = withPWA({
+//   reactStrictMode: true,
+//   pwa: {
+//     dest: "public",
+//     register: true,
+//     skipWaiting: true,
+//     disable: process.env.NODE_ENV === "development",
+//   },
+// });
+
+// const withPWA = require("next-pwa");
+
+// module.exports = withPWA({
+//   pwa: {
+//     dest: "public",
+//     register: true,
+//     skipWaiting: true,
+//   },
+// });
